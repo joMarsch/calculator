78a5ffd75422af277ef6fefd82f8d88ff884ebed
joMarsch <jonas.marschner@mailbox.org>
2023-10-25 22:49:04 +0200

    Finish number input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const numberButtons = document.querySelectorAll(".number-button");
 const acButton = document.querySelector("#ac-button");
+const display = document.querySelector(".display")
 const displayValue = document.querySelector("#display-value");
 
 // Global Varaibles
@@ -32,9 +33,18 @@ function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
 
+function updateDisplayValue(element) {
+    if (displayValue.textContent == "0") {
+        displayValue.textContent = "";
+    }
+
+    if (displayValue.offsetWidth < 9 / 10 * display.offsetWidth){
+        displayValue.textContent += element.textContent;
+    }
+}
 
 function initializeNumberButtons(buttonList) {
-    buttonList.forEach((element) => element.addEventListener("click", function (e) {displayValue.textContent += this.textContent}));
+    buttonList.forEach((element) => element.addEventListener("click", function () { updateDisplayValue(this) }));
 }
 
 function initializeAcButton(button) {
