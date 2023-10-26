3f10e249fbbbfd4cf40628021ca2f42d4c7eb6b7
jonas.marschner@mailbox.org <jonas.marschner@mailbox.org>
2023-10-26 12:23:17 +0200

    Add basic calc functionality

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Global Constants
 
+const allButtons = document.querySelectorAll("button");
 const numberButtons = document.querySelectorAll(".number-button");
+const operateButtons = document.querySelectorAll(".operate-button");
 const acButton = document.querySelector("#ac-button");
 const dotButton = document.querySelector("#dot-button");
 const display = document.querySelector(".display");
 const displayValue = document.querySelector("#display-value");
 
-// Global Varaibles
+// Global Variables
 
 let firstNumber;
 let secondNumber;
 let operator;
+let isCalculating = false;
+let isRefreshable = false;
 
 // Used Function
 
@@ -29,14 +33,42 @@ function multiply(firstNumber, secondNumber) {
 
 function divide(firstNumber, secondNumber) {
     if (secondNumber == 0) {
-        return "inf";
+        return "NaN";
     }
     return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    let result;
+    firstNumber = +firstNumber;
+    secondNumber = +secondNumber;
+    switch(operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+        default:
+            break;
+    }
+    return result;
 }
 
 function updateDisplayValue(element) {
     if (displayValue.textContent == "0") {
         displayValue.textContent = "";
+    }
+
+    if (isRefreshable) {
+        displayValue.textContent = "";
+        isRefreshable = false;
     }
 
     if (displayValue.offsetWidth < 9 / 10 * display.offsetWidth){
@@ -50,22 +82,74 @@ function addDecimalPoint() {
     }
 }
 
+function clear() {
+    displayValue.textContent = "0";
+    clearVariables();
+}
+
+function clearVariables() {
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    isCalculating = false;
+    isRefreshable = false;
+}
+
+function nanHandling() {
+    if(displayValue.textContent == "NaN") {
+        clear();
+    }
+}
+
+function operationLogic() {// TODO: Refactor for readability
+    if (isCalculating) {
+        secondNumber = displayValue.textContent;
+        result = operate(firstNumber, secondNumber, operator);
+
+        displayValue.textContent = result;
+
+        if (this.textContent == "=") {
+            isCalculating = false;
+        } else {
+            isCalculating = true;
+            firstNumber = displayValue.textContent;
+        }
+    } else {
+        firstNumber = displayValue.textContent;
+        isCalculating = true
+    }
+    operator = this.textContent;
+    isRefreshable = true;
+}
+
+function initializeAllButtons(buttonList) {
+    buttonList.forEach((element) => element.addEventListener("click", nanHandling))
+}
+
 function initializeNumberButtons(buttonList) {
-    buttonList.forEach((element) => element.addEventListener("click", function () { updateDisplayValue(this) }));
+    buttonList.forEach((element) => element.addEventListener("click", function () { 
+        updateDisplayValue(this) 
+    }));
 }
 
 function initializeAcButton(button) {
-    button.addEventListener("click", () => displayValue.textContent = "0");
+    button.addEventListener("click", clear);
 }
 
 function initializeDotButton(button) {
     button.addEventListener("click", addDecimalPoint);
 }
 
+function initializeOperateButtons(buttonList) {
+    buttonList.forEach((element) => element.addEventListener("click", operationLogic));
+}
+
 // Constructing the Main Function
 
 function main() {
+    initializeAllButtons(allButtons);
     initializeNumberButtons(numberButtons);
+    initializeOperateButtons(operateButtons);
     initializeAcButton(acButton);
     initializeDotButton(dotButton);
 }
